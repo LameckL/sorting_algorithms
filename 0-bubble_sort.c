e9541c122015402fa96b13c92ceec983f821fc72
LameckL <88710250+LameckL@users.noreply.github.com>
2023-10-24 15:07:58 +0100

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - func swaping two integers in an array
+ * @a:first int to swap
+ * @b: second int to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - func sorting an array of integers in ascending order
+ * @array: array of integers being sorted
+ * @size: array size
+ *
+ * Description: after each swap, an array is printed
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
